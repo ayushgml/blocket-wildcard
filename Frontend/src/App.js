d01fa77a5a7dda955778224cc96527ec05a020d1
Alok Mathur <73957024+alok27a@users.noreply.github.com>
2022-07-10 13:42:20 +0530

    Pending request and history section added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import Pending from "./pages/Pending";
+import History from "./pages/History";
 
 
 const App = () => {
@@ -13,8 +15,8 @@ const App = () => {
           <Route exact path="/" element={<Home />} />
           <Route exact path="dashboard">
             <Route index element={<Dashboard />} />
-            {/* <Route path="pending" element={<Pending />} />
-            <Route path="history" element={<History />} /> */}
+            <Route path="pending" element={<Pending />} />
+            <Route path="history" element={<History />} />
           </Route>
         </Routes>
       </BrowserRouter>
